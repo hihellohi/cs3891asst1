2528834b885a1c26997d07a2c7b276b6344a7377
Kevin <hihellohi610@gmail.com>
2017-03-23 00:18:22 +1100

    q3

--- kern/asst1/producerconsumer.c
+++ kern/asst1/producerconsumer.c
@@ -1,12 +1,19 @@
 /* This file will contain your solution. Modify it as you wish. */
 #include <types.h>
 #include "producerconsumer_driver.h"
+#include <synch.h>  /* for P(), V(), sem_* */
+#include <lib.h>  /* kassert */
 
 /* Declare any variables you need here to keep track of and
    synchronise your bounded. A sample declaration of a buffer is shown
    below. You can change this if you choose another implementation. */
 
 static struct pc_data buffer[BUFFER_SIZE];
+struct semaphore *full;
+struct semaphore *empty;
+struct lock *mutex;
+
+int hi, lo;
 
 
 /* consumer_receive() is called by a consumer to request more data. It
@@ -17,11 +24,14 @@ struct pc_data consumer_receive(void)
 {
         struct pc_data thedata;
 
-        (void) buffer; /* remove this line when you start */
+		P(full);
+		lock_acquire(mutex);
 
-        /* FIXME: this data should come from your buffer, obviously... */
-        thedata.item1 = 1;
-        thedata.item2 = 2;
+		thedata = buffer[lo++];
+
+		lock_release(mutex);
+		V(empty);
+
 
         return thedata;
 }
@@ -31,7 +41,13 @@ struct pc_data consumer_receive(void)
 
 void producer_send(struct pc_data item)
 {
-        (void) item; /* Remove this when you add your code */
+		P(empty);
+		lock_acquire(mutex);
+
+		buffer[hi++] = item;
+
+		lock_release(mutex);
+		V(full);
 }
 
 
@@ -42,10 +58,18 @@ void producer_send(struct pc_data item)
 
 void producerconsumer_startup(void)
 {
+	full = sem_create("full", 0);
+	empty = sem_create("empty", BUFFER_SIZE);
+	mutex = lock_create("mutex");
+	KASSERT(full != NULL && empty != NULL && mutex != NULL);
+	hi = lo = 0;
 }
 
 /* Perform any clean-up you need here */
 void producerconsumer_shutdown(void)
 {
+	sem_destroy(full);
+	sem_destroy(empty);
+	lock_destroy(mutex);
 }
 
